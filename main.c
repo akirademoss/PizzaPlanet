7d8630d1292d43b5b5cd74845ce5681df7a05c19
Akira DeMoss <akirademoss@gmail.com>
2017-12-05 05:05:54 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,40 @@
 #include <stdint.h>
-#include "lcd.h"
-#include "Timer.h"
-#include "button.h"
-#include "WiFi.h"
-#include "uart.h"
-#include "open_interface.h"
-#include "movement.h"
+#include <stdio.h>
+#include <inc/tm4c123gh6pm.h>
+#include <lcd.h>
+#include <Timer.h>
+#include <button.h>
+#include <WiFi.h>
+#include <uart.h>
+#include <open_interface.h>
+#include <movement.h>
+#include <string.h>
+#include <math.h>
+#include <servo.h>
+#include <ping.h>
+#include <IR.h>
+#include <scan.h>
+
+
 
 void UART_init(void);
 char UART_Receive(void);
 char uart_receive(void);
 
+
+// IMPORTANT: THIS IS CALIBRATED FOR CYBOT #2!!!!!!!!!!!
+// NOTE - the wifi password is the following: Z12345678
+// (TODO) - Final remaining logic includes evaluating object widths for mapping // playing with scan functions // tweaking // any scan logic
+// (TODO) - calibrate angle for CyBot 2
+// (TODO) - Map distance in a 2-D coordinate grid (e.g. declare 3 global variables in main; double totalAngle = N, double totalDistX = N, and double totalDistY = N),
+// then update functions in movement.c to return changes in each (note: may need to perform basic trig calculations)  and update distance each time the CyBot moves
+
+//Calibration for CyBot #2
+
 //Blocking call to receive over uart1
 //returns char with data
 //int last = 0;
-int main(void)
+main(void)
 {
     oi_t *sensor = oi_alloc();
     oi_init(sensor);
@@ -23,96 +43,69 @@ int main(void)
     lcd_init();
     UART_init();
     button_init();
-
-    char dispBuff[] = "testingg";
-    char *distPtr;
-    distPtr = dispBuff;
-
-    lcd_printf(distPtr);
-
-    auto_reroute(sensor,100);
+    WiFi_start();
+    adc_init();
+    servo_init();
+    ping_init();
 
 
+    //auto_reroute(sensor, 50);
 
-//This is a test for the signal threshholds of the cliff sensors
-//It will output the value of the sensor at a given position on the floor
-/*
-    while(1)
-    {
-        char msg[81] = "";
-        oi_update(sensor);
-        int threshhold = sensor->cliffLeftSignal;
-        if((sensor->cliffLeft) || ((sensor->cliffLeftSignal) > 0))
-        {
-            sprintf(msg, "Cliff on left side! %i \n\r", threshhold);
-            lcd_printf(msg);
-          }
-     }
-*/
+    //NOTE - uncomment this to test scanner
+    //while(1){
+    //timer_waitMillis(1000);
+    //scan_action();
+    //}
 
-
-
-//Ann Devin Code
+    //Ann Devin Code
     char buffer[21];
     int i = 0;
     int j = 0;
 
 
-    char pass[] = "annIsCool123";
     char command;
-    WiFi_start(pass);
     while(1){
         command = uart_receive();
         if(command != '\r'){
             lcd_printf("the char is: %c\n the index is %d", command, i);
             buffer[i] = command;
 
-            //Moves the bot forward or backward N centimeters
-            //e.g. "collision_avoidance(sensor, N);" --> if N = 15 moves forward 15cm.  if N = -15 moves backward 15cm
-            //Integrated CRATER avoidance and WHITE LINE avoidance
-            //When hits a CRATER or WHITE LINE moves backward 5cm and turns + or - 90 degrees
-            if(command == 'm')
+            if(command == 'w')
             {
                 auto_reroute(sensor,15);
             }
             //rotate 90 degrees clockwise
-            if(command == 'r')
+            if(command == 'f')
             {
                 turn(sensor, -90);
             }
             //rotate 45 degrees clockwise
-            if(command == 's')
+            if(command == 'd')
             {
                 turn(sensor, -45);
             }
             //rotate 90 degrees counter clockwise
-            if(command == 't')
+            if(command == 'a')
             {
                 turn(sensor, 90);
             }
             //rotate 45 degrees counter clockwise
-            if(command == 'u')
+            if(command == 's')
             {
                 turn(sensor, 45);
             }
-            if(command == 'v')
+            //move backwards 5cm
+            if(command == 'b')
             {
                 move(sensor,-5,250);
             }
-/*
-        } else {
-            buffer[i] = '\0';
-            i = 0;
-            lcd_printf("%s\n", buffer);
-            uart_sendChar('\n');
-            uart_sendChar('\r');
-            for(j = 0; j < 20; j++){
-               uart_sendChar(buffer[j]);
-             }
-*/
+            //scan with ping and IR
+            if(command == 'p')
+            {
+                scan_action();
+            }
         }
 
-
     }
-    return 0;
 }
+
