f6fe7a67fac5f845c44c0f8132e8e27a69700027
Akira DeMoss <akirademoss@gmail.com>
2017-12-05 05:08:11 -0600

    Create servo.c

---
+++ servo.c
@@ -0,0 +1,140 @@
+/*
+ * scan.c
+ *
+ *  Created on: Dec 5, 2017
+ *      Author: demoss
+ */
+
+#include <scan.h>
+#include <math.h>
+#include <IR.h>
+#include <ping.h>
+#include <servo.h>
+#include <Timer.h>
+#include <button.h>
+#include <uart.h>
+#include <WiFi.h>
+#include <stdbool.h>
+
+int16_t scan_degrees = 0;
+int8_t obj_count = 0;
+_Bool obj_detection = false;
+int16_t deg_start = 0;
+int16_t deg_end = 0;
+int16_t temp_deg_end = 0;
+int16_t deg_width = 0;
+float distance_total = 0;
+float distance_avg = 0;
+int16_t distance_count = 0;
+float object_width = 0;
+int16_t deg_smallest_obj = 0;
+float smallest_obj_width = 100;
+int8_t edge_count = 0;
+_Bool end_case = false;
+
+/// Resets all the global variables.
+/**
+ *      Resets all the global variables for a new scan. This allows for each scan to produce fresh results
+ *      that are not affected by previous scans.
+ *
+ */
+void scan_reset(){
+    scan_degrees = 0;
+    obj_detection = false;
+    deg_start = 0;
+    deg_end = 0;
+    temp_deg_end = 0;
+    deg_width = 0;
+    distance_total = 0;
+    distance_avg = 0;
+    distance_count = 0;
+    object_width = 0;
+    deg_smallest_obj = 0;
+    smallest_obj_width = 100;
+    edge_count = 0;
+    end_case = false;
+
+    // Sets servo back to reset zero, waits to prevent false readings
+    move_servo(0);
+    timer_waitMillis(250);
+}
+
+/// Scans from 0 degrees to 180 degrees and returns over UART objects detected and their approximate location.
+/**
+ *      Scans from 0 degrees to 180 degrees and returns over UART objects detected and their approximate location.
+ *      Sends back data in following format, space delimited:
+ *              ['S'][deg_start][deg_end][distance]
+ *
+ */
+void scan_action(){
+    scan_reset();
+
+    while(scan_degrees < 180){
+        float ir_dist = ir_getDistance();
+        float ping_dist = ping_read();
+
+
+        // Used to prevent false positives in detecting edge ends
+        if((ir_dist > 80 || ping_dist > 90) && obj_detection && !end_case) {
+            temp_deg_end = scan_degrees;
+            edge_count++;
+            end_case = true;
+        } else if((ir_dist > 80 || ping_dist > 90) && obj_detection && end_case) {
+            edge_count++;
+        }
+
+        // calls when a new object is spotted
+        if ( (ir_dist <= 80) && ping_dist <= 90 && !obj_detection) {
+            obj_detection = true;
+            deg_start = scan_degrees;
+            deg_end = 0;
+            obj_count++;
+            distance_avg = 0;
+
+            sprintf(buffer, "Object Spotted\r\n");
+            send_string(buffer);
+
+        }
+
+        // calls when an object is lost in sight
+        else if ( (ir_dist > 80 || ping_dist > 90) && obj_detection && edge_count > 1) {
+            obj_detection = false;
+            deg_end = temp_deg_end;
+            deg_width = abs(deg_end - deg_start); // calculate object width in degrees
+
+            object_width = ( distance_avg * sin((deg_width) * (PI/180) ) ) / ( sin(( (180-deg_width)/2) * (PI/180) ) );
+
+            if(object_width < smallest_obj_width){
+                smallest_obj_width = object_width;
+                deg_smallest_obj = deg_start;
+            }
+
+            // Transmit data over UART
+            int dist_send = (int) distance_avg;
+
+            sprintf(buffer, "S %d %d %d\r\n", deg_start, deg_end,dist_send);
+            send_string(buffer);
+
+            // Reset error protection
+            distance_count = distance_total = 0;
+            edge_count = 0;
+            end_case = false;
+        }
+
+        // Calculates running average of the distance from sensor to object
+        if(obj_detection){
+            distance_count++;
+            distance_total += ping_dist;
+            distance_avg = distance_total / distance_count;
+        }
+
+        // Increments servo by a degree
+        scan_degrees = scan_degrees + 2;
+        move_servo(scan_degrees);
+        timer_waitMillis(100);
+    }
+
+    move_servo(90);
+}
+
+
