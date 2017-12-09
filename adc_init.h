d7ad9510607200372a44fff126b670f593d88da1
Akira DeMoss <akirademoss@gmail.com>
2017-12-09 12:30:05 -0600

    Update and rename IR.h to adc_init.h

---
+++ adc_init.h
@@ -0,0 +1,5 @@
+/** * @file adc_init.h * @brief This file contains all functions related to our IR sensor * * @authors  Ann Gould, Akira DeMoss, Devin Uner, Takao Shibamoto * @date     Dec 7, 2017 */
+#ifndef ADC_INIT_H_#define ADC_INIT_H_
+/**  *Setup ADC for GPIO pin 4 port B, ADC module 0 on port D to enable IR sensor functionality  */void init_ADC();
+/** * Returns the distance of an object from the IR sensor * Calibration for CyBot #5 * @return distance:   Distance an object is relative to the IR sensor (accurate within 2cm over a range from 9 to 80cm) */int ir_stuff();
+
